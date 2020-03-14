4ac12f9a0157e94180a74886827a34e222ef62ce
Pedro Higuchi <higuchip@gmail.com>
2020-03-14 18:32:25 -0300

    Dados atualizados em 14/03/2020 às 16:25

--- analise.R
+++ analise.R
@@ -11,8 +11,8 @@ my.formula<-y~I(x^2)+I(x^3)
 m <- lm(my.formula, covid19_1)
 summary(m)
 m
-my.eq<-"3.917 - 0.330*x^2 + 0.0384*x^3"
-p<-ggplot(data=covid19, aes(t,casos)) + xlim(0,24)+ylim(0,400)+
+my.eq<-"3.910 - 0.329*x^2 + 0.0384*x^3"
+p<-ggplot(data=covid19, aes(t,casos)) + xlim(0,25)+ylim(0,500)+
   geom_point(alpha=0.7, shape=21, fill="red", colour="black", size=5) +
   geom_smooth(method="lm", se=TRUE, formula=y~I(x^2)+I(x^3), fullrange=TRUE)
 label.text <- paste(gsub("x", "~italic(x)", my.eq, fixed = TRUE),
@@ -20,7 +20,7 @@ label.text <- paste(gsub("x", "~italic(x)", my.eq, fixed = TRUE),
                           format(summary(m)$r.squared, digits = 2),
                           sep = "~`=`~"),
                     sep = "~~~~")
-p<-p+labs(y="Casos confirmados de COVID-19", x = "Dias desde o primeiro caso registrado de COVID-19", caption = "Fonte: Ministério da Saúde (Dados atualizados em 13/03/2020 às 15:45)") +
+p<-p+labs(y="Casos confirmados de COVID-19", x = "Dias desde o primeiro caso registrado de COVID-19", caption = "Fonte: Ministério da Saúde (Dados atualizados em 14/03/2020 às 16:25)") +
   ggtitle("COVID-19 no Brasil", subtitle = "Atenção: este é apenas um exercício didático, sem fins científicos")
 p<-p + theme_light(base_size = 14)
 jpeg(filename = "covid19.jpg",width = 2500, height = 2500,
