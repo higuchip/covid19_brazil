db624c0b4773523892fb87ed117d244fda0de30d
higuchip <higuchip@gmail.com>
2020-03-16 16:27:38 -0300

    Atualização Ministério da Saúde

--- analise.R
+++ analise.R
@@ -12,8 +12,8 @@ my.formula<-y~I(x^2)+I(x^3)
 m <- lm(my.formula, covid19_1)
 summary(m)
 m
-my.eq<-"8.2361 - 0.5817*x^2 + 0.0548*x^3"
-p<-ggplot(data=covid19, aes(t,casos)) +
+my.eq<-"9.8462 - 0.6667*x^2 + 0.0601*x^3"
+p<-ggplot(data=covid19, aes(t,casos)) + ylim(0,350)+
   geom_point(alpha=0.7, shape=21, fill="red", colour="black", size=5) +
   geom_smooth(method="lm", se=TRUE, formula=y~I(x^2)+I(x^3), fullrange=TRUE)
 label.text <- paste(gsub("x", "~italic(x)", my.eq, fixed = TRUE),
@@ -21,10 +21,10 @@ label.text <- paste(gsub("x", "~italic(x)", my.eq, fixed = TRUE),
                           format(summary(m)$r.squared, digits = 2),
                           sep = "~`=`~"),
                     sep = "~~~~")
-p<-p+labs(y="Casos confirmados de COVID-19", x = "Dias desde o primeiro caso registrado de COVID-19", caption = "Fonte: Ministério da Saúde (Dados atualizados em 15/03/2020 às 19:15)") +
+p<-p+labs(y="Casos confirmados de COVID-19", x = "Dias desde o primeiro caso registrado de COVID-19", caption = "Fonte: Ministério da Saúde (Dados atualizados em 16/03/2020 às 15:15)") +
   ggtitle("COVID-19 no Brasil", subtitle = "Atenção: este é apenas um exercício didático, sem fins científicos")
 p<-p + theme_light(base_size = 14)
-p<-p + annotate(geom = "text", x = 0, y = 150, label = label.text,
+p<-p + annotate(geom = "text", x = 0, y = 350, label = label.text,
                 family = "serif", hjust = 0, parse = TRUE, size =6)
 
 jpeg(filename = "covid19.jpg",width = 2500, height = 2500,
@@ -33,8 +33,8 @@ jpeg(filename = "covid19.jpg",width = 2500, height = 2500,
      bg = "white",
      res = 300, family="serif")
 
-p+annotate(geom = "curve", x = 19, y = 250, xend = 19, yend = 200, 
+p+annotate(geom = "curve", x = 15, y = 280, xend = 20, yend = 234, 
   curvature = 0, arrow = arrow(length = unit(2, "mm"))) +
-  annotate(geom = "text", x = 17, y = 255, label = "15/03/20", hjust = "left", cex=6)
+  annotate(geom = "text", x = 10, y = 285, label = "16/03/20", hjust = "left", cex=6)
 
 dev.off()
