52faf481ca845144e248a0c04534081c1856f0f9
Pedro Higuchi <higuchip@gmail.com>
2020-03-15 23:44:11 -0300

    Dados atualizados em 15/03/2020 às 19:15

--- analise.R
+++ analise.R
@@ -12,8 +12,8 @@ my.formula<-y~I(x^2)+I(x^3)
 m <- lm(my.formula, covid19_1)
 summary(m)
 m
-my.eq<-"7.4834 - 0.5378*x^2 + 0.05194*x^3"
-p<-ggplot(data=covid19, aes(t,casos)) + xlim(0,26)+ylim(0,650)+
+my.eq<-"8.2361 - 0.5817*x^2 + 0.0548*x^3"
+p<-ggplot(data=covid19, aes(t,casos)) + xlim(0,26)+ylim(0,700)+
   geom_point(alpha=0.7, shape=21, fill="red", colour="black", size=5) +
   geom_smooth(method="lm", se=TRUE, formula=y~I(x^2)+I(x^3), fullrange=TRUE)
 label.text <- paste(gsub("x", "~italic(x)", my.eq, fixed = TRUE),
@@ -21,14 +21,20 @@ label.text <- paste(gsub("x", "~italic(x)", my.eq, fixed = TRUE),
                           format(summary(m)$r.squared, digits = 2),
                           sep = "~`=`~"),
                     sep = "~~~~")
-p<-p+labs(y="Casos confirmados de COVID-19", x = "Dias desde o primeiro caso registrado de COVID-19", caption = "Fonte: Ministério da Saúde (Dados atualizados em 15/03/2020 às 18:10)") +
+p<-p+labs(y="Casos confirmados de COVID-19", x = "Dias desde o primeiro caso registrado de COVID-19", caption = "Fonte: Ministério da Saúde (Dados atualizados em 15/03/2020 às 19:15)") +
   ggtitle("COVID-19 no Brasil", subtitle = "Atenção: este é apenas um exercício didático, sem fins científicos")
 p<-p + theme_light(base_size = 14)
+p<-p + annotate(geom = "text", x = 0, y = 600, label = label.text,
+                family = "serif", hjust = 0, parse = TRUE, size =6)
+
 jpeg(filename = "covid19.jpg",width = 2500, height = 2500,
      units = "px",
      quality = 100,
      bg = "white",
      res = 300, family="serif")
-p + annotate(geom = "text", x = 0, y = 350, label = label.text,
-             family = "serif", hjust = 0, parse = TRUE, size =6)
+
+p+annotate(geom = "curve", x = 19, y = 300, xend = 19, yend = 200, 
+  curvature = 0, arrow = arrow(length = unit(2, "mm"))) +
+  annotate(geom = "text", x = 18, y = 315, label = "Hoje", hjust = "left", cex=6)
+
 dev.off()
