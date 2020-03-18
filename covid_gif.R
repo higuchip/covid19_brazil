fd0c59ec52071df265c0a2e4de5bb545a4fbac02
Pedro Higuchi <higuchip@gmail.com>
2020-03-18 20:12:24 -0300

    atualizacao

--- covid_gif.R
+++ covid_gif.R
@@ -10,18 +10,22 @@ library(gifski)
 
 covid_confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
 head(covid_confirmed)
+colnames(covid_confirmed)
 
 dim(covid_confirmed)
 
 
 paises <- c("Brazil", "Italy")
-tempo<-c(1:55)
+
 
 covid_confirmed_subset<-covid_confirmed %>% filter(`Country/Region` %in% paises)
+head(covid_confirmed_subset)
 
+covid_confirmed_subset_df<-as.data.frame(t(covid_confirmed_subset[,5:60]))
+head(covid_confirmed_subset_df)
 
-covid_confirmed_subset_df<-as.data.frame(t(covid_confirmed_subset[,5:59]))
 colnames(covid_confirmed_subset_df)<-c("Italia", "Brasil")
+tempo<-c(1:56)
 covid_confirmed_subset_df$tempo<-tempo
 covid_df_long<-covid_confirmed_subset_df %>% gather(pais, casos, Italia:Brasil)
 
@@ -35,11 +39,10 @@ p_brasil<-ggplot(covid_df_long_brasil, aes(tempo, casos, group = pais)) +
   geom_line(size = 2, color='green') +transition_reveal(tempo)+
   geom_point(size = 5, color='green')+
   view_follow(fixed_y = c(0.0001,NA))+
-  labs(title = 'Brasil', y = 'Casos Covid-19')+
+  labs(title = 'Brasil - 17/03/2020', y = 'Casos Covid-19')+
   theme_minimal(base_size = 22) + 
   theme(plot.margin = margin(5.5, 40, 5.5, 5.5))
 gif_brasil<-animate(p_brasil, fps=4, width = 400, height = 400,  renderer = gifski_renderer(loop = F))
-
 anim_save("brasil.gif", gif_brasil)
 
 
@@ -48,7 +51,7 @@ p_italia<-ggplot(covid_df_long_italia, aes(tempo, casos, group = pais)) +
   geom_line(size = 2, color='red') +transition_reveal(tempo)+
   geom_point(size = 5, color='red')+
   view_follow(fixed_y = c(0.0001,NA))+
-  labs(title = 'Itália', y = 'Casos Covid-19')+
+  labs(title = 'Itália - 17/03/2020', y = 'Casos Covid-19')+
   theme_minimal(base_size = 22) + 
   theme(plot.margin = margin(5.5, 40, 5.5, 5.5))
 gif_italia<-animate(p_italia, fps=4, width = 400, height = 400,  renderer = gifski_renderer(loop = F))
