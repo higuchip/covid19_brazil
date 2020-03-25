660776c9c7a8f8bfb69b2469cb6f279b7f15a26b
Pedro Higuchi <higuchip@gmail.com>
2020-03-25 00:04:07 -0300

    atualizacao 24/03/2020

--- covid_gif_100.R
+++ covid_gif_100.R
@@ -9,7 +9,8 @@ library(gifski)
 
 ###Dados extraídos de Novel Coronavirus (COVID-19) Cases, provided by JHU CSSE https://systems.jhu.edu/research/public-health/ncov/
 
-covid_confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+#today<-read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv')
+covid_confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 head(covid_confirmed)
 colnames(covid_confirmed)
 
@@ -17,28 +18,43 @@ colnames(covid_confirmed)
 covid_confirmed_brasil <-covid_confirmed %>% filter(`Country/Region` == 'Brazil')
 covid_confirmed_brasil <-covid_confirmed_brasil[,5:dim(covid_confirmed_brasil)[2]]
 dim(covid_confirmed_brasil)
-covid_confirmed_brasil[62]<-1891 #atualizacao ministério da saúde
+#covid_confirmed_brasil[62]<-1924 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+
+
+
+covid_confirmed_eua <-covid_confirmed %>% filter(`Country/Region` == 'US')
+covid_confirmed_eua <-covid_confirmed_eua[,5:dim(covid_confirmed_eua)[2]]
+covid_confirmed_eua<-apply(covid_confirmed_eua,2,sum)
+#today_usa<- today %>% group_by(Country_Region) %>%  summarise(casos = sum(Confirmed)) %>% filter(Country_Region=="US")
+#today_usa[2]
+#covid_confirmed_eua[62]<-43667
 
 
 covid_confirmed_italia <-covid_confirmed %>% filter(`Country/Region` == 'Italy')
 covid_confirmed_italia <-covid_confirmed_italia[,5:dim(covid_confirmed_italia)[2]]
 dim(covid_confirmed_italia)
+#covid_confirmed_italia[62]<-63927	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+
 
 
 covid_confirmed_japao <-covid_confirmed %>% filter(`Country/Region` == 'Japan')
 covid_confirmed_japao <-covid_confirmed_japao[,5:dim(covid_confirmed_japao)[2]]
 dim(covid_confirmed_japao)
+#covid_confirmed_japao[62]<-1128	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+
 
 
 covid_confirmed_korea_s <-covid_confirmed %>% filter(`Country/Region` == 'Korea, South')
 covid_confirmed_korea_s <-covid_confirmed_korea_s[,5:dim(covid_confirmed_korea_s)[2]]
 dim(covid_confirmed_korea_s)
+#covid_confirmed_korea_s[62]<-8961	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
 
 
 
-covid_confirmed_paises <- rbind(covid_confirmed_korea_s, covid_confirmed_brasil, covid_confirmed_italia, covid_confirmed_japao)
+
+covid_confirmed_paises <- rbind(covid_confirmed_korea_s, covid_confirmed_brasil, covid_confirmed_eua, covid_confirmed_italia, covid_confirmed_japao)
 covid_confirmed_paises<-t(covid_confirmed_paises)
-colnames(covid_confirmed_paises)<-c('Coreia S.','Brasil','Italia', 'Japao')
+colnames(covid_confirmed_paises)<-c('Coreia S.','Brasil','EUA','Italia', 'Japao')
 
 
 covid_confirmed_paises_long<-as.data.frame(covid_confirmed_paises) %>% gather(pais, casos)
@@ -48,15 +64,16 @@ covid_long_100 <- covid_confirmed_paises_long %>% filter(casos>99)
 
 tempo_100_coreia<-1:dim(covid_long_100 %>% filter(pais=='Coreia S.'))[1]
 tempo_100_brasil<-1:dim(covid_long_100 %>% filter(pais=='Brasil'))[1]
+tempo_100_eua<-1:dim(covid_long_100 %>% filter(pais=='EUA'))[1]
 tempo_100_italia<-1:dim(covid_long_100 %>% filter(pais=='Italia'))[1]
 tempo_100_japao<-1:dim(covid_long_100 %>% filter(pais=='Japao'))[1]
 
-covid_long_100$tempo_100<-c(tempo_100_coreia,tempo_100_brasil, tempo_100_italia,tempo_100_japao)
+covid_long_100$tempo_100<-c(tempo_100_coreia,tempo_100_brasil, tempo_100_eua, tempo_100_italia,tempo_100_japao)
 
 p_gif_100 <- ggplot(covid_long_100, aes(x=tempo_100, y=casos, group=pais)) +
   scale_y_log10()+
-  ggtitle("COVID-19", subtitle = "Brasil, Coreia do Sul, Itália e Japão após o centésimo caso")+
-  labs(y="Casos confirmados (log10)", x = "Tempo (dias)", caption = "Fonte dos dados: Johns Hopkins CSSE - 23/03/2020")+
+  ggtitle("COVID-19 - Numéro acumulado de casos confirmados", subtitle = "Trajeatória após o centésimo caso")+
+  labs(y="Casos confirmados (log10)", x = "Tempo (dias)", caption = "Fonte dos dados: Johns Hopkins CSSE - 24/03/2020")+
   geom_line(size=2,aes(linetype=pais, color=pais)) +
   geom_segment(aes(xend=max(tempo_100), yend = casos), linetype=2, colour='black') +
   geom_point(size = 3) + 
--- deaths.R
+++ deaths.R
@@ -9,7 +9,7 @@ library(gifski)
 
 ###Dados extraídos de Novel Coronavirus (COVID-19) Cases, provided by JHU CSSE https://systems.jhu.edu/research/public-health/ncov/
 
-deaths_confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+deaths_confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 head(deaths_confirmed)
 colnames(deaths_confirmed)
 
@@ -17,32 +17,40 @@ colnames(deaths_confirmed)
 deaths_confirmed_brasil <-deaths_confirmed %>% filter(`Country/Region` == 'Brazil')
 deaths_confirmed_brasil <-deaths_confirmed_brasil[,5:dim(deaths_confirmed_brasil)[2]]
 dim(deaths_confirmed_brasil)
-deaths_confirmed_brasil[62]<-34 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+#deaths_confirmed_brasil[62]<-34 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+
+deaths_confirmed_eua <-deaths_confirmed %>% filter(`Country/Region` == 'US')
+deaths_confirmed_eua <-deaths_confirmed_eua[,5:dim(deaths_confirmed_eua)[2]]
+deaths_confirmed_eua<-apply(deaths_confirmed_eua,2,sum)
+today_usa_deaths<- today %>% group_by(Country_Region) %>%  summarise(casos = sum(Deaths)) %>% filter(Country_Region=="US")
+#today_usa_deaths[2]
+#deaths_confirmed_eua[62]<-552
+
 
 deaths_confirmed_italia <-deaths_confirmed %>% filter(`Country/Region` == 'Italy')
 deaths_confirmed_italia <-deaths_confirmed_italia[,5:dim(deaths_confirmed_italia)[2]]
 dim(deaths_confirmed_italia)
-deaths_confirmed_italia[62]<-6077	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+#deaths_confirmed_italia[62]<-6077	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
 
 
 
 deaths_confirmed_japao <-deaths_confirmed %>% filter(`Country/Region` == 'Japan')
 deaths_confirmed_japao <-deaths_confirmed_japao[,5:dim(deaths_confirmed_japao)[2]]
 dim(deaths_confirmed_japao)
-deaths_confirmed_japao[62]<-42	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+#deaths_confirmed_japao[62]<-42	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
 
 
 
 deaths_confirmed_korea_s <-deaths_confirmed %>% filter(`Country/Region` == 'Korea, South')
 deaths_confirmed_korea_s <-deaths_confirmed_korea_s[,5:dim(deaths_confirmed_korea_s)[2]]
 dim(deaths_confirmed_korea_s)
-deaths_confirmed_korea_s[62]<-111	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
+#deaths_confirmed_korea_s[62]<-111	 #from https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv
 
 
 
-deaths_confirmed_paises <- rbind(deaths_confirmed_korea_s, deaths_confirmed_brasil, deaths_confirmed_italia, deaths_confirmed_japao)
+deaths_confirmed_paises <- rbind(deaths_confirmed_korea_s, deaths_confirmed_brasil, deaths_confirmed_eua, deaths_confirmed_italia, deaths_confirmed_japao)
 deaths_confirmed_paises<-t(deaths_confirmed_paises)
-colnames(deaths_confirmed_paises)<-c('Coreia S.','Brasil','Italia', 'Japao')
+colnames(deaths_confirmed_paises)<-c('Coreia S.','Brasil','EUA','Italia', 'Japao')
 
 
 deaths_confirmed_paises_long<-as.data.frame(deaths_confirmed_paises) %>% gather(pais, casos)
@@ -52,15 +60,16 @@ deaths_long_1 <- deaths_confirmed_paises_long %>% filter(casos>10)
 
 tempo_deaths_coreia<-1:dim(deaths_long_1 %>% filter(pais=='Coreia S.'))[1]
 tempo_deaths_brasil<-1:dim(deaths_long_1 %>% filter(pais=='Brasil'))[1]
+tempo_deaths_eua<-1:dim(deaths_long_1 %>% filter(pais=='EUA'))[1]
 tempo_deaths_italia<-1:dim(deaths_long_1 %>% filter(pais=='Italia'))[1]
 tempo_deaths_japao<-1:dim(deaths_long_1 %>% filter(pais=='Japao'))[1]
 
-deaths_long_1$tempo_1<-c(tempo_deaths_coreia,tempo_deaths_brasil, tempo_deaths_italia,tempo_deaths_japao)
+deaths_long_1$tempo_1<-c(tempo_deaths_coreia,tempo_deaths_brasil, tempo_deaths_eua, tempo_deaths_italia,tempo_deaths_japao)
 
 death_gif_1 <- ggplot(deaths_long_1, aes(x=tempo_1, y=casos, group=pais)) +
   scale_y_log10()+
-  ggtitle("COVID-19", subtitle = "Brasil, Coreia do Sul, Itália e Japão após décima mortalidade")+
-  labs(y="Número de Mortos (log10)", x = "Tempo (dias)", caption = "Fonte dos dados: Johns Hopkins CSSE - 23/03/2020")+
+  ggtitle("COVID-19 - Mortalidade acumulada", subtitle = "Trajetória após a décima mortalidade")+
+  labs(y="Número de Mortos (log10)", x = "Tempo (dias)", caption = "Fonte dos dados: Johns Hopkins CSSE - 24/03/2020")+
   geom_line(size=2,aes(linetype=pais, color=pais)) +
   geom_segment(aes(xend=max(tempo_1), yend = casos), linetype=2, colour='black') +
   geom_point(size = 3) + 
@@ -74,4 +83,4 @@ death_gif_1 <- ggplot(deaths_long_1, aes(x=tempo_1, y=casos, group=pais)) +
   theme(plot.margin = margin(1.0, 40, 1.0, 1.0),legend.position = "none")
 
 gif_deaths<-animate(death_gif_1, fps=4, duration = 20, renderer = gifski_renderer(loop = F))
-anim_save("paises_deaths_caso_100.gif", gif_deaths)
+anim_save("paises_deaths_caso_10.gif", gif_deaths)
